689df743a12c252c26ae95d3cada7fc911dc0ff0
ItsEley <itseley09@gmail.com>
2022-10-04 23:15:56 +0800

    Edited home.php and Added home.css

---
+++ home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home | PESO Nasugbu</title>
+    <link rel="stylesheet" type="text/css" href="home.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light" id="navbar-main">
+            <a href="#" class="navbar-brand mx-3">
+                <img src="images\Bossjob.svg" alt="LOGO" srcset="" height="29" width="120">
+            </a>
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#contentMenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse " id="contentMenu">
+                <ul class="navbar-nav mx-5">
+
+                    <li class="nav-item active">
+                      <a href="#" class="nav-link ">Jobs</a></li>
+                    <li class="nav-item ">
+                      <a href="#" class="nav-link" >Companies</a></li>
+                    <li class="nav-item ">
+                      <a href="#" class="nav-link" >Career Guide</a></li>
+
+                    <li class="nav-item ">
+                      <a href="#" class="nav-link">I'm a Jobseeker</a></li>
+                    <li class="nav-item">
+                    <a href="#" class="nav-link">I'm an Employer</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
