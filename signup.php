44fe444b0eaee74b406e8f7d3d0849126686f310
ItsEley <itseley09@gmail.com>
2022-10-04 20:11:04 +0800

    Added home.php

--- signup.php
+++ signup.php
@@ -4,38 +4,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign Up</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="style-signup.css" rel="stylesheet" />
+    <title>Signup Employee | PESO Nasugbu</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style-signup.css">    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="container">
-        <p class="login-text"></p>
-        <div class="login-social">\
-            <a href="#"><i class="fa fa-facebook">Facebook</i></a>
-            <a href="#"><i class="fa fa-twitter">Twitter</i></a>
-            <a href="#"><i class="fa fa-google">Google</i></a>
-            <a href="#"><i class="fa fa-facebook">Facebook</i></a>
-        </div>
-        <form action="login-email">
-            <p class="login-text">Login with Email</p>
-            <div class="input-group">
-                <input type="email" placeholder="Email" require>
-            </div>
-            <div class="input-group">
-                <input type="password" placeholder="Password" require>
-            </div>
-            <div class="input-group">
-                <button class="btn">Login</button>
+    <div class="form">
+        <h2>Sign Up</h2>
+        <p>It's free and always will be.</p>
+        <form action="">
+            <div class="error-text">Error</div>
+            <div class="grid-details">
+                <div class="input">
+                    <label for="">First Name</label>
+                    <input type="text" name="fname" placeholder="First Name" require pattern="[a-zA-Z'-'\s]*">
+                </div>
+                <div class="input">
+                    <label for="">Last Name</label>
+                    <input type="text" name="lname" placeholder="Last Name" require pattern="[a-zA-Z'-'\s]*">
+                </div>
+                <div class="input">
+                    <label for="">Email</label>
+                    <input type="email" name="email" placeholder="Email" require>
+                </div>
+                <div class="input">
+                    <label for="">Phone</label>
+                    <input type="phone" name="phone" placeholder="Phone Number" require pattern="[0-9]{11}" oninvalid="this.setCustomValidity('Enter Valid Phone Number')" oninput="this.setCustomValidity('')">
+                </div>
+                <div class="grid-details">
+                    <div class="input">
+                        <label for="">Password</label>
+                        <input type="password" name="pass" placeholder="Password" require>
+                    </div>
+                    <div class="input">
+                        <label for="">Confirm Password</label>
+                        <input type="password" name="cpass" placeholder="Confirm Password" require>
+                    </div>
+                </div>
+                <div class="btn">
+                    <input type="submit" value="Signup Now" class="btn-primary">
+                </div>
+                <div class="link">Already signed up ? <a href="#">Login now!</a></div>
+               
             </div>
         </form>
+        
     </div>
-    <?php
-    
-    echo"System";
-    
-    ?>
-    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </html>
