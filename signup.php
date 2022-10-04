82c25f420e4ea7e10c7f1a757c2d8419cd318d57
ItsEley <itseley09@gmail.com>
2022-10-04 14:49:19 +0800

    Added files in signup.php and style-signup.css

--- signup.php
+++ signup.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="style-signup.css" rel="stylesheet" />
 </head>
 <body>
     <div class="container">
@@ -21,8 +22,19 @@
             <div class="input-group">
                 <input type="email" placeholder="Email" require>
             </div>
+            <div class="input-group">
+                <input type="password" placeholder="Password" require>
+            </div>
+            <div class="input-group">
+                <button class="btn">Login</button>
+            </div>
         </form>
     </div>
+    <?php
+    
+    echo"System";
+    
+    ?>
     
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
