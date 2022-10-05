28fba527eeaf2dc6e3cf7179adfe86343767c56d
ItsEley <itseley09@gmail.com>
2022-10-05 11:19:07 +0800

    Edited home.php and home.css

--- home.php
+++ home.php
@@ -5,15 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home | PESO Nasugbu</title>
-    <link rel="stylesheet" type="text/css" href="home.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" type="text/css" href="home.css">
 </head>
 <body>
 <div class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-light" id="navbar-main">
             <a href="#" class="navbar-brand mx-3">
-                <img src="images\Bossjob.svg" alt="LOGO" srcset="" height="29" width="120">
+                <img src="https://assets.bossjob.com/jobseeker/Bossjob.svg" alt="LOGO" srcset="" height="29" width="120">
             </a>
             
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#contentMenu">
@@ -36,8 +35,18 @@
                     <a href="#" class="nav-link">I'm an Employer</a></li>
                 </ul>
             </nav>
+            <div class="sub-navbar row mx-auto" id="sub-nav">
+              <div class="form">
+                <input id="searchj"type="text" name="searchj" value="<?php echo isset($_GET['searchj']) ? $_GET['searchj'] : ''; ?>" placeholder="Search for job title or company name">
+                <input class="autocomplete" action="/action_page.php"id="searchl"type="text" name="searchl" value="<?php echo isset($_GET['searchl']) ? $_GET['searchl'] : ''; ?>" placeholder="Location">
+                
+                <button id="searchq-btn" class="btn btn-primary" type="submit" value="search" name="">Search</button>
+              </div>
+            </div>
+           
         </div>
     </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
